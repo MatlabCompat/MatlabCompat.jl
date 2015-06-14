bd95761788e6ec41b9996e82505c1bf9e5eb77aa
Artur Yakimovich <artur.yakimovich@imls.uzh.ch>
2015-06-15 00:07:43 +0200

    added support module and few wrappers for the image module

--- src/MatlabCompat.jl
+++ src/MatlabCompat.jl
@@ -1,16 +1,26 @@
 module MatlabCompat
 
-export Image
+export Image, Support
 export graythresh,
-       im2bw
+       im2bw,
+       imshow,
+       imread,
+       mat2im,
+       im2mat,
+       rossetta
+
 
 # Include submodules of the package.
-for file in split("image")
+for file in split("image support")
     include("MatlabCompat/$file.jl")
 end
 
 # Alias some functions.
 const graythresh = Image.graythresh
 const im2bw = Image.im2bw
-
+const  imshow = Image.imshow
+const  imread = Image.imread
+const  mat2im = Image.mat2im
+const  im2mat = Image.im2mat
+const  rossetta = Image.rossetta
 end # module
